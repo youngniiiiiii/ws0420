3cd15866e1e41c4d14d7b4fab4f6d76bcfcb764a
youngeunkwon <youngnikk4929@gmail.com>
2023-04-20 17:08:45 +0900

    Initial commit

---
+++ src/main/java/com/kbstar/Ws0420Application.java
@@ -0,0 +1,24 @@
+package com.kbstar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@SpringBootApplication
+public class Ws0420Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ws0420Application.class, args);
+    }
+
+    @Bean
+    public InternalResourceViewResolver setupViewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/views/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
+
+
+}
